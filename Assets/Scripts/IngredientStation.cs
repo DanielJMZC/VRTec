aa154485f1956247dcd432e9a16ffbddf237a3f0
Iván Gabriel Espinosa García <a01178743@tec.mx>
2026-04-21 11:42:43 -0600

    Initial commit

---
+++ Assets/Scripts/IngredientStation.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class IngredientStation : MonoBehaviour
+{
+    public GameObject ingredientPrefab;
+    private XRSimpleInteractable interactable;
+
+    void OnEnable()
+    {
+        interactable = GetComponent<XRSimpleInteractable>();
+        if (interactable != null)
+            interactable.selectEntered.AddListener(OnGrabStation);
+    }
+
+    void OnDisable()
+    {
+        if (interactable != null)
+            interactable.selectEntered.RemoveListener(OnGrabStation);
+    }
+
+    void OnGrabStation(SelectEnterEventArgs args)
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+
+        GameObject ingredient = Instantiate(ingredientPrefab, 
+        args.interactorObject.transform.position,
+        args.interactorObject.transform.rotation);
+
+        XRGrabInteractable grab = ingredient
+        .GetComponent<XRGrabInteractable>();
+        
+        if (grab != null)
+            args.manager.SelectEnter(args.interactorObject, grab);
+    }
+}
