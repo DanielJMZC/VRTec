6dbc4a29a33b636e0d505af149bede8be6f42843
DanielJMZC <A01723532@tec.mx>
2026-04-22 06:02:18 -0600

    Main

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,8 +7,23 @@ public class GameController : MonoBehaviour
     public Transform ClientTarget;
     public Transform ClientDestination;
 
+     public Behaviour moveProvider; // Continuous Move Provider
+    public Behaviour teleportProvider;
+
     void Awake()
     {
         Instance = this;
+    }
+
+       void Start()
+    {
+        moveProvider.enabled = false;
+        teleportProvider.enabled = false;
+    }
+
+    public void EnableMovement()
+    {
+        moveProvider.enabled = true;
+        teleportProvider.enabled = true;
     }
 }
